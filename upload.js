9355367c524d52908932da78ac0df83294e6f665
Vandals fashion Offical <vandalsfashionofficial@gmail.com>
2026-04-09 19:54:25 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -78,7 +78,7 @@ form.addEventListener("submit", async (e) => {
   price,
   category,
   description,
-  images: imageUrls,
+  imageUrls: imageUrls,
   displayOn,
   inStock: true, // 🔥 for Step 8 later
   createdAt: Timestamp.now()
